9ea31af5bb3ab75107d2ef552323e6b9f9e95760
Rafael Felix Correa <rafael.correa@olx.com>
2019-02-04 16:30:32 +0100

    function_name is saved using the ARN when using the amazon lambda console

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_permission" "cloudwatch_trigger" {
     count         = "${var.attach_cloudwatch_rule_config ? 1 : 0}"
     statement_id  = "AllowExecutionFromCloudWatch"
     action        = "${lookup(var.cloudwatch_rule_config, "enabled", true) ? "lambda:InvokeFunction" : "lambda:DisableInvokeFunction"}"
-    function_name = "${element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_s3.*.function_name, aws_lambda_function.lambda_with_dl.*.function_name, aws_lambda_function.lambda_with_vpc.*.function_name, aws_lambda_function.lambda_with_dl_and_vpc.*.function_name), 0)}"
+    function_name = "${element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_s3.*.arn, aws_lambda_function.lambda_with_dl.*.arn, aws_lambda_function.lambda_with_vpc.*.arn, aws_lambda_function.lambda_with_dl_and_vpc.*.arn), 0)}"
     principal     = "events.amazonaws.com"
     source_arn    = "${aws_cloudwatch_event_rule.rule.arn}"
 }
